eb16aa2440ef07bf0a2153897d9781fae6ad9f9a
onmokoworks <onmokoworks@gmail.com>
2026-07-18 23:14:49 +0900

    game: match KartItem mode 3 pre callback

--- configure.py
+++ configure.py
@@ -386,6 +386,7 @@ config.libs = [
             Object(Matching, "game/KartItem_Stub_Returns0.c"),
             Object(Matching, "game/KartItem_RenderCb_Mode7_Pre.c", extra_cflags=["-Cpp_exceptions on"]),
             Object(Matching, "game/KartItem_RenderCb_DefaultCycle_Pre.c", extra_cflags=["-Cpp_exceptions on"]),
+            Object(Matching, "game/KartItem_RenderCb_Mode3_Pre.c", extra_cflags=["-Cpp_exceptions on"]),
             Object(Matching, "game/EffectState_HasContent.c"),
             Object(Matching, "game/EffectSteering_InputViscosity_SetFieldC.c"),
             Object(Matching, "game/EffectActor_BindKartTargetPair.c"),
---
+++ src/game/KartItem_RenderCb_Mode3_Pre.c
@@ -0,0 +1,6 @@
+extern void fn_802C352C(int);
+
+void KartItem_RenderCb_Mode3_Pre(void)
+{
+    fn_802C352C(0);
+}
