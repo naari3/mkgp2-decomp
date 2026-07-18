24799eacc43af90d2667ba5a30037db17a9374e0
onmokoworks <onmokoworks@gmail.com>
2026-07-18 22:34:48 +0900

    game: match ItemEffectDamp initializer

---
+++ src/game/ItemEffectDamp_Init.c
@@ -0,0 +1,13 @@
+extern const float lbl_806D27F8;
+
+void ItemEffectDamp_Init(void *self)
+{
+    float *fields = (float *)self;
+
+    fields[2] = lbl_806D27F8;
+    fields[3] = lbl_806D27F8;
+    fields[4] = lbl_806D27F8;
+    fields[5] = lbl_806D27F8;
+    fields[6] = lbl_806D27F8;
+    ((int *)self)[7] = 0;
+}
