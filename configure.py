3b985e4695ef4fb27660f71b76610f9db067bd78
onmokoworks <onmokoworks@gmail.com>
2026-08-11 02:02:27 +0900

    Integrate CoinSystem UpdateAll metadata

--- configure.py
+++ configure.py
@@ -352,6 +352,7 @@ config.libs = [
             Object(Matching, "game/RaceContext_Dtor.c", extab_padding=b"\x00\x00"),
             Object(Matching, "game/RaceContext_InitDefaults.c", extab_padding=b"\x00\x00"),
             Object(Matching, "game/RaceContext_EnsureSingleton.c", extab_padding=b"\x00\x00"),
+            Object(Matching, "game/CoinSystem_UpdateAll.c", extab_padding=b"\x00\x00"),
                     Object(Matching, "game/auto_800A8F4C_block.c", extab_padding=b"\x00\x00"),
             Object(Matching, "game/auto_ONKARTHIT_block.c", extab_padding=b"\x00\x00", extra_cflags=["-use_lmw_stmw on", "-Cpp_exceptions on"]),
             Object(Matching, "game/CObj_GlobalProjPushFrame.c", extab_padding=b""),
