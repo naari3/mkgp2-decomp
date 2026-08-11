217ab9564a1f14138a5498c8ac0285b448a1583a
onmokoworks <onmokoworks@gmail.com>
2026-08-11 17:05:33 +0900

    Promote RaceContext destructor to C++

--- src/game/RaceContext_Dtor.c
+++ src/game/RaceContext_Dtor.c
@@ -5,10 +5,18 @@
 /* --- extern decls: branch callees (bl/b targets) --- */
 /* Open prototype (`extern void Foo();`) accepts any call signature; */
 /* refine if the real prototype matters for header consumers. */
-extern void MemoryManager_TimedFree();
+extern void MemoryManager_TimedFree(void *);
 
 /* --- extern decls: sda21-referenced data --- */
-extern unsigned int lbl_806D10A0;
+#pragma cplusplus on
+struct PolymorphicOwner {
+    virtual ~PolymorphicOwner();
+};
+struct RaceContextView {
+    void *vtable;
+};
+extern PolymorphicOwner *lbl_806D10A0;
+#pragma cplusplus off
 
 /* --- extern decls: large-data refs (@ha/@l pairs) --- */
 /* Open array (`[]`) avoids sda21 strict-mode link errors when a future */
@@ -21,7 +29,9 @@ extern unsigned int lbl_8040E4DC[];
  */
 
 /* --- forward decls --- */
-asm void RaceContext_Dtor(void);
+#pragma cplusplus on
+extern "C" void *RaceContext_Dtor(RaceContextView *, short);
+#pragma cplusplus off
 
 /* --- extab (manual emit, .extab_user -> extab via objcopy) --- */
 #pragma section R ".extab_user"
@@ -36,47 +46,22 @@ __declspec(section ".extabindex_user") static const struct { void *fn; unsigned 
 };
 
 /* --- asm function bodies (.text order = fn address order) --- */
-asm void RaceContext_Dtor(void) { /* 0x8009D638 size:0x94 */
-    nofralloc
-    stwu r1, -0x10(r1)
-    mflr r0
-    stw r0, 0x14(r1)
-    stw r31, 0xc(r1)
-    mr r31, r4
-    stw r30, 0x8(r1)
-    mr. r30, r3
-    beq RaceContext_Dtor_L_8009D6B0
-    lis r3, lbl_8040E4D0@ha
-    addi r0, r3, lbl_8040E4D0@l
-    stw r0, 0x0(r30)
-    lwz r3, lbl_806D10A0(r13)
-    cmplwi r3, 0x0
-    beq RaceContext_Dtor_L_8009D684
-    lwz r12, 0x0(r3)
-    li r4, 0x1
-    lwz r12, 0x8(r12)
-    mtctr r12
-    bctrl
-    RaceContext_Dtor_L_8009D684:
-    li r0, 0x0
-    cmplwi r30, 0x0
-    stw r0, lbl_806D10A0(r13)
-    beq RaceContext_Dtor_L_8009D6A0
-    lis r3, lbl_8040E4DC@ha
-    addi r0, r3, lbl_8040E4DC@l
-    stw r0, 0x0(r30)
-    RaceContext_Dtor_L_8009D6A0:
-    extsh. r0, r31
-    ble RaceContext_Dtor_L_8009D6B0
-    mr r3, r30
-    bl MemoryManager_TimedFree
-    RaceContext_Dtor_L_8009D6B0:
-    lwz r0, 0x14(r1)
-    mr r3, r30
-    lwz r31, 0xc(r1)
-    lwz r30, 0x8(r1)
-    mtlr r0
-    addi r1, r1, 0x10
-    blr
+#pragma cplusplus on
+#pragma exceptions off
+extern "C" void *RaceContext_Dtor(RaceContextView *self, short freeFlag) { /* 0x8009D638 size:0x94 */
+    if (self != 0) {
+        self->vtable = (void *)lbl_8040E4D0;
+        delete lbl_806D10A0;
+        lbl_806D10A0 = 0;
+        if (self != 0) {
+            self->vtable = (void *)lbl_8040E4DC;
+        }
+        if (freeFlag > 0) {
+            MemoryManager_TimedFree(self);
+        }
+    }
+    return self;
 }
+#pragma exceptions reset
+#pragma cplusplus off
 
